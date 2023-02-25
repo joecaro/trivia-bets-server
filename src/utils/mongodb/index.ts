7920e12f63f68e9ecc35219794b43f8e9145283a
Joseph Carothers <jsc11598@gmail.com>
2023-02-24 19:48:14 -0500

    debugging log

--- src/utils/mongodb/index.ts
+++ src/utils/mongodb/index.ts
@@ -3,6 +3,9 @@ import { MongoClient } from 'mongodb';
 import { GameState } from '../../types';
 import { UserClient } from '../../types';
 
+console.log('MONGO_DB_URL', process.env.MONGO_DB_URL);
+
+
 export const client = new MongoClient(process.env.MONGO_DB_URL as string);
 const dbName = 'trivia-bets';
 
